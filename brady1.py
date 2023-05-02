c766e429471b8f4e128bccd2ba2c2548aafe5563
Brady Underwood <underwood.brady@gmail.com>
2023-05-02 09:25:43 -0600

    update

--- brady1.py
+++ brady1.py
@@ -4,4 +4,8 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
 dbs = DBSCAN(eps=0.26, min_samples=10);
+#dbs.fit(pca_raisin);
+
+#plt.scatter(pca_raisin[:,0],pca_raisin[:,1], c=dbs.labels_)
